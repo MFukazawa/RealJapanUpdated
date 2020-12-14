64ce92591ac80310a2c803ae2f0fdfbbaec8f39c
MFukazawa <micahfukazawa@gmail.com>
2020-12-14 15:33:13 +0900

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// import { app } from './styles/app.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-5xl my-6'>Real Japan</h1>
+
+      <main className='w-screen text-center'>
+
+        <div className='p-4'>Enter your total price and area (m²)</div>
+        <label htmlFor='price' />
+        <input
+          type='text'
+          name='price'
+          id='price'
+          className='border border-black rounded mx-4 p-2'
+        />
+        <label htmlFor='area' />
+        <input
+          type='text'
+          name='area'
+          id='area'
+          className='border border-black rounded mx-4 p-2'
+        />
+
+        <div className='w-screen'>
+          <div className='p-4'>
+            Enter your Prefecture, City, Neighborhood, and Property Type
+          </div>
+
+          <div className='flex flex-wrap justify-center items-center'>
+            <label htmlFor='prefecture' />
+            <input
+              type='text'
+              name='prefecture'
+              id='prefecture'
+              className='border border-black rounded m-4 p-2'
+            />
+
+            <label htmlFor='city' />
+            <input
+              type='text'
+              name='city'
+              id='city'
+              className='border border-black rounded m-4 p-2'
+            />
+
+            <label htmlFor='neighborhood' />
+            <input
+              type='text'
+              name='neighborhood'
+              id='neighborhood'
+              className='border border-black rounded m-4 p-2'
+            />
+
+            <label htmlFor='property-type' />
+            <input
+              type='text'
+              name='property-type'
+              id='property-type'
+              className='border border-black rounded m-4 p-2'
+            />
+          </div>
+        </div>
+        <button className="border rounded bg-blue-400 text-white px-10 py-2 mt-8">Search</button>
+      </main>
     </div>
   );
 }
