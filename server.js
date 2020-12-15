6cc347a539258693a525c3b9887cc9e8c9d2cd98
MFukazawa <micahfukazawa@gmail.com>
2020-12-15 18:11:42 +0900

    test: update server

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const user = {
-    name: "John",
-    hobby: "skating"
-  }
-  response.setHeader('Content-Type', 'application/json');
-  response.end(JSON.stringify(user));
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('<h1>Hi</h1>');
+  next();
 });
 
-server.listen(3000);
+app.get('/', (req, res) => {
+  res.send('getting root');
+});
+
+app.get('/profile', (req, res) => {
+  res.send('getting profile');
+});
+
+app.post('/profile', (req, res) => {
+  const user = {
+    name: 'John',
+    hobby: 'skating',
+  };
+  res.send(user);
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening at http://localhost:${port}`);
+});
+
+// const http = require('http');
+
+// const server = http.createServer((request, response) => {
+//   const user = {
+//     name: "John",
+//     hobby: "skating"
+//   }
+//   response.setHeader('Content-Type', 'application/json');
+//   response.end(JSON.stringify(user));
+// });
+
+// server.listen(3000);
