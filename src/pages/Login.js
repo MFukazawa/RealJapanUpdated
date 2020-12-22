ad98c48eea4fd91242557065368cdafc21086eab
MFukazawa <micahfukazawa@gmail.com>
2020-12-22 17:54:38 +0900

    style: update nav + width overflow

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Switch, Route, Link } from 'react-router-dom'
 import classes from '../styles/login.module.css';
 
 export default function Report() {
   return (
     <div className={classes.Login}>
       <div className={classes.loginHero}>
+        <nav className="flex justify-end w-screen p-5 fixed">
+          <Link to="/" className="hover:text-blue-500">Home</Link>
+        </nav>
+
         <div className="flex justify-center items-center h-screen">
           <div className="flex flex-col justify-center items-center w-1/2 py-10 shadow-2xl border border-white rounded-3xl">
-            <h1 className='text-5xl pt-6'>Login</h1>
+            <h1 className='text-5xl pt-6 tracking-widest'>Login</h1>
             <label className="mt-6">Email</label>
             <input className="border border-black rounded px-5 py-3" />
             <label className="mt-6">Password</label>
