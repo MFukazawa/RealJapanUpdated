7fe91c03e45ceae5ef7ff35afd393fb42c5f263d
MFukazawa <micahfukazawa@gmail.com>
2020-12-23 12:02:59 +0900

    feat: update login to pull from /login api, change to form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
-import React, { useState }from 'react';
-import { Link, useHistory } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import classes from '../styles/login.module.css';
 
 const Login = (props) => {
@@ -10,47 +10,70 @@ const Login = (props) => {
   // });
   let history = useHistory();
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = () => {
-    fetch('http://localhost:9027')
-      .then((res) => res.json())
-      .then(console.log)
-    history.push('/')
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      fetch('http://localhost:9027/login', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data === 'success') {
+            history.push('/');
+          }
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div className={classes.Login}>
       <div className={classes.loginHero}>
-        <nav className="flex justify-end w-screen p-5 fixed">
-          <Link to="/" className="hover:text-blue-500">Home</Link>
+        <nav className='flex justify-end w-screen p-5 fixed'>
+          <Link to='/' className='hover:text-blue-500'>
+            Home
+          </Link>
         </nav>
 
-        <div className="flex justify-center items-center h-screen">
-          <div className="flex flex-col justify-center items-center w-1/2 py-10 bg-white bg-opacity-70 shadow-2xl border border-white rounded-3xl">
+        <div className='flex justify-center items-center h-screen'>
+          <form
+            onSubmit={handleSubmit}
+            className='flex flex-col justify-center items-center w-1/2 py-10 bg-white bg-opacity-70 shadow-2xl border border-white rounded-3xl'
+          >
             <h1 className='text-5xl pt-6 tracking-widest'>Login</h1>
-            <label className="mt-6">Email</label>
+            <label className='mt-6'>Email</label>
             <input
               value={email}
-              type="email"
+              type='email'
               onChange={(e) => setEmail(e.target.value)}
-              className="border border-black bg-transparent rounded px-5 py-3" />
-            <label className="mt-6">Password</label>
+              className='border border-black bg-transparent rounded px-5 py-3'
+            />
+            <label className='mt-6'>Password</label>
             <input
               value={password}
-              type="password"
+              type='password'
               onChange={(e) => setPassword(e.target.value)}
-              className="border border-black bg-transparent rounded px-5 py-3" />
+              className='border border-black bg-transparent rounded px-5 py-3'
+            />
             <input
-              onClick={handleSubmit}
-              className="border border-black bg-transparent cursor-pointer hover:opacity-50 rounded mt-6 px-5 py-2"
-              type="submit"
-              value="Login" />
-          </div>
+              className='border border-black bg-transparent cursor-pointer hover:opacity-50 rounded mt-6 px-5 py-2'
+              type='submit'
+              value='Login'
+            />
+          </form>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Login;
